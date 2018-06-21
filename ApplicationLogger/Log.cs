56dee086527fb222ee9161e4bba7d6494c590bf2
lucavanzin <35525463+lucavanzin@users.noreply.github.com>
2018-06-21 14:07:23 +0200

    Update Log.cs

--- ApplicationLogger/Log.cs
+++ ApplicationLogger/Log.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LaserIndustries.ApplicationLogger
+namespace LucaVanzin.ApplicationLogger
 {
 
     public static class Logger
